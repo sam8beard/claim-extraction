cd7986bf20b5c8cfe020ebca74bee3f44fd92d1e
sam8beard <sammybeard2002@gmail.com>
2025-06-22 21:56:37 -0400

    s3 upload for files complete

--- go/cmd/uploader/main.go
+++ go/cmd/uploader/main.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"path/filepath"
 	"time"
+	"os"
 )
 
 func main() { 
@@ -29,23 +30,36 @@ func main() {
 
 	fmt.Println(filePath, source)
 
+	// connect to client
 	client, err := s3client.NewClient() 
 	if err != nil { 
 		panic(err)
 	} // if
 	
+	// get file reader for upload
+	fileReader, err := os.Open(filePath)
+	if err != nil { 
+		panic(err)
+	} // if 
+	defer fileReader.close() 
+
+	// make file key for upload 
 	fileName := filepath.Base(filePath)
 	currTime := time.Now() 
 	formattedTime := currTime.Format(time.RFC3339)
-
-
 	fileKey := fmt.Sprint(source, "/", formattedTime, "_", fileName)
 
+	// link client to uploader
 	uploader := manager.NewUploader(client)
 	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
 		Bucket: aws.String("claim-pipeline-docstore")
 		Key: 	aws.String(fileKey)
-		Body: // file reader
+		Body:	fileReader
 	})
-	
+
+	if err != nil { 
+		panic(err)
+	} // if 
+
+	fmt.Println("Successfully uploaded to: ", result)
 } // main
