506bce6554936e99369a328c942f3f4329aca528
sam8beard <sammybeard2002@gmail.com>
2025-06-22 21:19:22 -0400

    began parsing two required flags for upload tool

--- go/cmd/uploader/main.go
+++ go/cmd/uploader/main.go
@@ -1,9 +1,26 @@
-package uploader 
+package main
 
 import ( 
-
+	"fmt"
+	"flag"
 )
 
 func main() { 
+	var filePath string
+	var source string 
+
+	// identify which flags to look for - user must enter file path and source
+	flag.StringVar(&filePath, "file", "", "Path to the document")
+	flag.StringVar(&source, "source", "", "Source of the document")
+
+	// parse flags provided 
+	flag.Parse()
+
+	if filePath == "" || source == "" { 
+		fmt.Println("Must provide file path and source to use")
+		return
+	} // if 
+
+	fmt.Println(filePath, source)
 
 } // main
