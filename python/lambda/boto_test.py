e3f9dc1e2f0644fe00781aec4d1040cbf1aee810
sam8beard <sammybeard2002@gmail.com>
2025-07-08 02:00:29 -0400

    added testing for event parsing

--- python/lambda/boto_test.py
+++ python/lambda/boto_test.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 s3 = boto3.resource('s3')
 
+# prints all buckets in s3 instance 
 for bucket in s3.buckets.all(): 
     print(bucket.name)
+
--- python/lambda/extract_text_lambda.py
+++ python/lambda/extract_text_lambda.py
@@ -1,3 +1,25 @@
-import boto3
+import boto3, os, json, urllib.parse
+from dotenv import load_dotenv
+
+# load env vars
+load_dotenv()
+
+# connect to s3 client
+# s3 = boto3.client('s3') # COMMENTED OUT FOR NOW WHILE TESTING
+
+# FOR TESTING: load sample event for parsing
+
+def main(): 
+    test_file = open('sample_event.json', 'r')
+    data = json.load(test_file)
+    handler(data, None)
+
 def handler(event, context): 
-    print()
+    # get an object from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
+    print(bucket, key)
+
+if __name__ == '__main__': 
+    main()
