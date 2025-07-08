7c3a8fed2aa1615252d0231b0cae1b4b569865e0
sam8beard <sammybeard2002@gmail.com>
2025-07-08 02:26:22 -0400

    implementing testing for s3 file download

--- python/lambda/extract_text_lambda.py
+++ python/lambda/extract_text_lambda.py
@@ -5,21 +5,26 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # connect to s3 client
-# s3 = boto3.client('s3') # COMMENTED OUT FOR NOW WHILE TESTING
-
-# FOR TESTING: load sample event for parsing
+s3 = boto3.client('s3') 
 
 def main(): 
-    test_file = open('sample_event.json', 'r')
-    data = json.load(test_file)
-    handler(data, None)
+    test_file_download()
 
+# testing boto3 file downloading 
+def test_file_download(): 
+    try:
+        s3.download_file('claim-pipeline-docstore', 'test/2025-06-24T15:09:44-04:00_basic-text.pdf', 'output.txt')
+    except Exception as e: 
+        print(e)
+
+# main handler for extraction lambda
 def handler(event, context): 
     # get an object from the event
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
     print(bucket, key)
+
 
 if __name__ == '__main__': 
     main()
