37e0d75d27458354a9f0a30840821fefcff3ce02
sam8beard <sammybeard2002@gmail.com>
2025-10-22 18:05:58 -0400

    added additional util script

--- python/conversion/test_conversion.py
+++ python/conversion/test_conversion.py
@@ -4,55 +4,56 @@ import psycopg2
 from utils import extract_text
 from utils import get_pdfs
 from utils import upload_text
+from utils import create_clients
 import os
 import pprint
 from dotenv import load_dotenv
 
-def create_clients(): 
-    '''
-    Loads environment variables and creates MinIO client 
-    and PostgreSQL client.
+# def create_clients(): 
+#     '''
+#     Loads environment variables and creates MinIO client 
+#     and PostgreSQL client.
 
-    Return: 
-        - minio_client: MinIO client
-        - pg_client: PostgreSQL client
-    '''
+#     Return: 
+#         - minio_client: MinIO client
+#         - pg_client: PostgreSQL client
+#     '''
 
-    minio_client, pg_client = None, None
+#     minio_client, pg_client = None, None
 
-    try: 
-        # load env vars
-        load_dotenv()
+#     try: 
+#         # load env vars
+#         load_dotenv()
 
-        # create clients
-        db_params = { 
-            "host": os.getenv("DB_HOST"),
-            "database": os.getenv("DB_NAME"),
-            "user": os.getenv("DB_USERNAME"), 
-            "password": os.getenv("DB_PASSWORD"), 
-            "port": os.getenv("DB_PORT")
-        }
-        minio_params = {
-            "endpoint": os.getenv("MINIO_ENDPOINT"),
-            "access_key": os.getenv("MINIO_USER"),
-            "secret_key": os.getenv("MINIO_PASSWORD"),
-            "secure": False
-        }
+#         # create clients
+#         db_params = { 
+#             "host": os.getenv("DB_HOST"),
+#             "database": os.getenv("DB_NAME"),
+#             "user": os.getenv("DB_USERNAME"), 
+#             "password": os.getenv("DB_PASSWORD"), 
+#             "port": os.getenv("DB_PORT")
+#         }
+#         minio_params = {
+#             "endpoint": os.getenv("MINIO_ENDPOINT"),
+#             "access_key": os.getenv("MINIO_USER"),
+#             "secret_key": os.getenv("MINIO_PASSWORD"),
+#             "secure": False
+#         }
        
-        pg_client = psycopg2.connect(**db_params)
-        minio_client = Minio(**minio_params)
+#         pg_client = psycopg2.connect(**db_params)
+#         minio_client = Minio(**minio_params)
        
 
-    except Exception as e: 
-        minio_client, ps_client = e, e
-        traceback = e.with_traceback()
+#     except Exception as e: 
+#         minio_client, ps_client = e, e
+#         traceback = e.with_traceback()
     
-    minio_success = not isinstance(minio_client, Exception)
-    pg_success = not isinstance(pg_client, Exception) 
+#     minio_success = not isinstance(minio_client, Exception)
+#     pg_success = not isinstance(pg_client, Exception) 
 
-    assert minio_success and pg_success, traceback
+#     assert minio_success and pg_success, traceback
 
-    return minio_client, pg_client
+#     return minio_client, pg_client
     
 def main(): 
     '''
@@ -61,7 +62,7 @@ def main():
 
     '''
 
-    minio_client, pg_client = create_clients()
+    minio_client, pg_client = create_clients.create_clients()
 
     pdfs = get_pdfs.get_pdf_files(minio_client)
 
@@ -83,8 +84,6 @@ def main():
         
         print(success)
 
-        # if success: 
-        #     print(success)
 
 
     pg_client.close()
