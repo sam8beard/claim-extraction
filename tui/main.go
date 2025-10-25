5b14142195002eaad09481a5e0714e20c1b4ecee
sam8beard <sammybeard2002@gmail.com>
2025-10-25 03:11:09 -0400

    completed additional practice

--- tui/main.go
+++ tui/main.go
@@ -11,25 +11,38 @@ import (
 	"fmt"
 )
 
-const url = "https://charm.sh"
+const charmUrl = "https://charm.sh"
+const googleUrl = "https://google.com"
 
 type model struct {
-	status int
-	err    error
+	status     int
+	err        error
+	receivedAt time.Time
+	state      string
+	url        string
+	choice     string
 } // model
 
 type statusMsg int
 type errMsg struct{ err error }
 
+// type stateMsg string
+
+// type readyMsg tea.Msg
+
 // for msgs that contain errors its usually handy to also implement
 // the error interface on the message
 func (e errMsg) Error() string { return e.err.Error() }
 
+func initialModel() model {
+	return model{state: "landing"}
+} // initialModel
+
 // this acts as a custom Cmd
-func checkServer() tea.Msg {
+func checkCharmServer() tea.Msg {
 	// create an http client and make get request
 	c := &http.Client{Timeout: 10 * time.Second}
-	res, err := c.Get(url)
+	res, err := c.Get(charmUrl)
 	if err != nil {
 		return errMsg{err}
 	} // if
@@ -38,50 +51,118 @@ func checkServer() tea.Msg {
 	// return the http status code as a msg
 	return statusMsg(res.StatusCode)
 
-} // checkServer
+} // checkCharmServer
+
+func checkServer(url string) tea.Cmd {
+	return func() tea.Msg {
+		c := &http.Client{Timeout: 10 * time.Second}
+		res, err := c.Get(url)
+		if err != nil {
+			return errMsg{err}
+		}
+		return statusMsg(res.StatusCode)
+	}
+}
+
+func checkGoogleServer() tea.Msg {
+	// create an http client and make get request
+	c := &http.Client{Timeout: 10 * time.Second}
+	res, err := c.Get(googleUrl)
+	if err != nil {
+		return errMsg{err}
+	} // if
+
+	// we received a response from the server
+	// return the http status code as a msg
+	return statusMsg(res.StatusCode)
+} // checkBadServer
 
 func (m model) Init() tea.Cmd {
 	// return the Cmd we made earlier
 	// NOTE: the function is not called; the bubble tea runtime
 	// 		 will do that when the time is right
-	return checkServer
-	// in bubbletea, Cmds return msgs
-	// checkServer IS a Cmd that returns a msg
-	// this Cmd's functionality is specified in the checkServer() function
 
+	// in bubbletea, Cmds return msgs
+	// checkCharmServer IS a Cmd that returns a msg
+	// this Cmd's functionality is specified in the checkCharmServer() function
+	return nil
 } // Init
 
-// func NewPrompt(prompt string) Prompt {
-// 	return Prompt{prompt: prompt}
-// }
+//	func NewPrompt(prompt string) Prompt {
+//		return Prompt{prompt: prompt}
+//	}
+// func ready() tea.Msg {
+// 	return readyMsg("ready")
+// } // ready
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case statusMsg:
-		// the server returned a statusMsg
-		// convert the statusMsg into an int
-		// save the new int into the int var of the m struct
-		// return the model and quit
-		m.status = int(msg)
-		return m, tea.Quit
-	case errMsg:
-		// there was an error
-		// save the errMsg into the err value of the m struct
-		m.err = msg
-		return m, tea.Quit
+	if msg != nil {
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			m.state = "loading"
+			switch msg.String() {
+			case "enter":
+				m.choice = "Charm"
+				return m, checkCharmServer
+			case " ":
+				m.choice = "Google"
+				return m, checkGoogleServer
+			case "q":
+				return m, tea.Quit
 
-	case tea.KeyMsg:
-		// we received a key press
-		// if the key press is ctrl+c, return the model and quit the program
-		if msg.Type == tea.KeyCtrlC {
+			case "b": // testing return on error
+				return m, checkServer("https://harm.sh")
+			} // switch
+		case statusMsg:
+			m.status = int(msg)
+			m.receivedAt = time.Now()
+			return m, nil
+		case errMsg:
+			m.err = msg
 			return m, tea.Quit
-		} // if
-	} // switch
+		default:
+			return m, nil
+		} // switch
+
+	} // if
+
+	return m, nil
+	// switch msg := msg.(type) {
+	// case statusMsg:
+	// 	// the server returned a statusMsg
+	// 	// convert the statusMsg into an int
+	// 	// save the new int into the int var of the m struct
+	// 	// return the model and quit
+	// 	m.state = "get"
+	// 	m.status = int(msg)
+	// 	m.receivedAt = time.Now()
+	// 	return m, initialModel
+	// case errMsg:
+	// 	// there was an error
+	// 	// save the errMsg into the err value of the m struct
+	// 	m.err = msg
+	// 	return m, tea.Quit
+	// case tea.KeyMsg:
+	// 	// we received a key press
+	// 	// if the key press is ctrl+c, return the model and quit the program
+
+	// 	if msg.Type == tea.KeyCtrlC {
+	// 		return m, tea.Quit
+	// 	} // if
+
+	// 	if msg.Type == tea.KeyEnter {
+	// 		return m, checkCharmServer
+	// 	}
+
+	// 	if msg.Type == tea.KeySpace {
+	// 		return m, checkGoogleServer
+	// 	}
+	// } // switch
 
 	// if we happen to get any other messages, just return the model
 	// the model is the current state of our application, and we don't
 	// want to change it in this case
-	return m, nil
+
 } // Update
 
 // look at our current model and build
@@ -92,20 +173,29 @@ func (m model) View() string {
 		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
 	} // if
 
-	// tell the user we're doing something
-	s := fmt.Sprintf("Checking %s ...\n", url)
+	s := "\nPress enter for Charm and space for Google\n"
 
-	// when the server responds with a status, add it to the current line
 	if m.status > 0 {
-		s += fmt.Sprintf("%d %s", m.status, http.StatusText(m.status))
-	} // if
+		s += fmt.Sprintf("\n%s queried...", m.choice)
+		s += fmt.Sprintf("%d %s \nReceived at: %s", m.status, http.StatusText(m.status), m.receivedAt)
+	}
+
+	// // tell the user we're doing something
+	// s += fmt.Sprintf("Checking %s ...\n", charmUrl)
+
+	// // when the server responds with a status, add it to the current line
+	// if m.status > 0 {
+	// 	s += fmt.Sprintf("%d %s \nReceived at: %s", m.status, http.StatusText(m.status), m.receivedAt)
+	// } // if
 
 	// send off whatever we came up with above for rendering
 	return "\n" + s + "\n\n"
 } // View
 
 func main() {
-	_, err := tea.NewProgram(model{}).Run()
+	p := tea.NewProgram(initialModel())
+
+	_, err := p.Run()
 
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
