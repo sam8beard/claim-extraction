a3bc6f1038d89c318c358028c2acaa63c45affbb
sam8beard <sammybeard2002@gmail.com>
2025-11-24 20:57:27 -0500

    restructured project

--- go-tui/backend/types/processing.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import (
-	"tui/backend/types/shared"
-)
-
-type ProcessingInput struct {
-	ConvertedFiles []shared.File
-}
--- go/uploader/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x 
-
-go mod tidy
-go build -o upload
